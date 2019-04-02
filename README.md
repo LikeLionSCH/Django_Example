16cc5e1b2f8bd10488d5633153a14582e652a703
alstn2468 <alstn2468_@naver.com>
2019-04-02 18:01:42 +0900

    Add django lotto example

--- README.md
+++ README.md
@@ -15,9 +15,12 @@ Department of<br/> Computer Software Engineering
 
 #### 1. Hello World! [[프로젝트]](https://github.com/LikeLionSCH/Django_Example/tree/master/firstproject)
 
+<br/>
 <img src="/result_image/helloworld.png" width="400" height="auto">
 
 
 #### 2. Lotto [[프로젝트]](https://github.com/LikeLionSCH/Django_Example/tree/master/secondproject)
 
-<img src="/result_image/lotto.png" width="400" height="auto" alt="아직 안만듬">
+<br/>
+<img src="/result_image/lotto1.png" width="300" height="auto" alt="아직 안만듬">
+<img src="/result_image/lotto2.png" width="300" height="auto" alt="아직 안만듬">
---
+++ secondproject/lotto/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+
+def home(request):
+    lotto = []
+    random_num = random.randint(1, 46)
+
+    for i in range(7):
+        while random_num in lotto:
+            random_num = random.randint(1, 46)
+
+        lotto.append(random_num)
+
+    lotto.sort()
+
+    return render(request, "home.html", {
+        "first": lotto[0],
+        "second": lotto[1],
+        "third": lotto[2],
+        "fourth": lotto[3],
+        "fifth": lotto[4],
+        "sixth": lotto[5],
+        "bonus": lotto[6],
+    })
