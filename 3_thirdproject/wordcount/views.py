7e6b566ceb45e8b7889cf44ce8dd237cbed26ec8
alstn2468 <alstn2468_@naver.com>
2019-04-03 18:52:14 +0900

    fix views.py result function

--- 3_thirdproject/wordcount/views.py
+++ 3_thirdproject/wordcount/views.py
@@ -14,6 +14,21 @@ def about(request):
     })
 
 def result(request):
-    return render(request, "result.html", {
-        
+    text = request.GET['fulltext']
+    words = text.split()
+    words_dic = {}
+
+    for word in words:
+        if word in words_dic.keys():
+            # Increase word count
+            words_dic[word] += 1
+
+        else:
+            # Add to dictionary
+            words_dic[word] = 1
+
+    return render(request, 'result.html', {
+        'full': text,
+        'total': len(words),
+        'dictionary': words_dic.items(),
     })
