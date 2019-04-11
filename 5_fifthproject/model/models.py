00a8a1b8f88f8581d831ef2448f8b056110af3df
alstn2468 <alstn2468_@naver.com>
2019-04-11 16:49:11 +0900

    Add student model in fifthproject

--- 5_fifthproject/model/models.py
+++ 5_fifthproject/model/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    major = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+    age = models.IntegerField()
