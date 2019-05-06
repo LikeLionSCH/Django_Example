ef6db5a6a329cf607e6cd66eb7aab20400983c93
alstn2468 <alstn2468_@naver.com>
2019-05-06 15:03:33 +0900

    Model timezone error Fix

--- 7_seventhproject/crud/models.py
+++ 7_seventhproject/crud/models.py
@@ -5,7 +5,22 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=20)
     body = models.TextField()
-    time = models.DateTimeField
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def preview(self):
+        return self.body[:50]
+
+    def created_at_to_str(self):
+        date_format = '%Y년 %m월 %d일 %H시 %M분'
+
+        return self.created_at.strftime(date_format)
+
+    def updated_at_to_str(self):
+        date_format = '%Y년 %m월 %d일 %H시 %M분'
+
+        return self.updated_at.strftime(date_format)
--- 7_seventhproject/crud/views.py
+++ 7_seventhproject/crud/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {
+    posts = Post.objects.all
 
+    return render(request, "index.html", {
+        "posts": posts,
     })
 
 
 def new(request):
     return render(request, "new.html", {
-        
+
     })
 
 
 def create(request):
+    post = Post()
+
+    post.title = request.GET("title")
+    post.body = request.GET("body")
+    post.time = timezone.localtime()
+
+    post.save()
+
     return redirect("index")
